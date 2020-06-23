d782e4c087a6ae2ab0528e900b2483f6af485291
nashvincent <36297311+nashvincent@users.noreply.github.com>
2020-06-23 12:54:13 +0530

    Updates

---
+++ precache-manifest.9d54ba50d7fcebee4054cd6f45e3dd38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fd47e608ef413d4d289a7bde48b2f9",
+    "url": "/dashboard-frontend/index.html"
+  },
+  {
+    "revision": "f8b011a8252b94efe13d",
+    "url": "/dashboard-frontend/static/css/2.d0625acf.chunk.css"
+  },
+  {
+    "revision": "3fc1e66ae1adbb93cc32",
+    "url": "/dashboard-frontend/static/css/main.3d3cea1e.chunk.css"
+  },
+  {
+    "revision": "f8b011a8252b94efe13d",
+    "url": "/dashboard-frontend/static/js/2.e4f7fa1b.chunk.js"
+  },
+  {
+    "revision": "d4d333c99f0d5d841c7cf4dd84511b8a",
+    "url": "/dashboard-frontend/static/js/2.e4f7fa1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc1e66ae1adbb93cc32",
+    "url": "/dashboard-frontend/static/js/main.f88e4e4a.chunk.js"
+  },
+  {
+    "revision": "68bdf14d3e053a433e99",
+    "url": "/dashboard-frontend/static/js/runtime-main.838ebe04.js"
+  }
+]);
